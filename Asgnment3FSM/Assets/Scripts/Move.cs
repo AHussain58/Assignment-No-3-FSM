9bcb50af76936890c74cf204effd8f9f5a43880f
Abbas Hussain <59785902+Hussain432@users.noreply.github.com>
2020-06-05 20:45:33 +0500

    Added Project

---
+++ Asgnment3FSM/Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] float speed=45.0f;
+    [SerializeField] float turnspeed = 20.0f;
+    private float horizontalInput;
+    private float forwordInput;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwordInput = Input.GetAxis("Vertical");
+
+        
+        transform.Translate(Vector3.forward*speed*Time.deltaTime*forwordInput);
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime); 
+
+    }
+}
